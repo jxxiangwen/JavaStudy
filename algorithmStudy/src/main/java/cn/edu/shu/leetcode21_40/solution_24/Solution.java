142436d50afe28bbe02319adaa3c7e859e044be3
jxxiangwen <jxxiangwen@live.com>
2016-10-12 12:25:56 +0800

    add readme

--- algorithmStudy/src/main/java/cn/edu/shu/leetcode21_40/solution_24/Solution.java
+++ algorithmStudy/src/main/java/cn/edu/shu/leetcode21_40/solution_24/Solution.java
@@ -3,28 +3,38 @@ package cn.edu.shu.leetcode21_40.solution_24;
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
  * }
  */
 
 class ListNode {
-    private int val;
+    int val;
     ListNode next;
+
     ListNode(int x) {
         this.val = x;
     }
 }
+
+/**
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * <p>
+ * For example,
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ * <p>
+ * Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+ */
 public class Solution {
     public ListNode swapPairs(ListNode head) {
-        if(head == null || head.next == null){
+        if (head == null || head.next == null) {
             return head;
         }
         ListNode listNode = new ListNode(0);
         listNode.next = head;
         ListNode pivot = listNode;
-        while(pivot.next != null && pivot.next.next != null){
+        while (pivot.next != null && pivot.next.next != null) {
             ListNode temp = pivot.next;
             pivot.next = temp.next;
             pivot = pivot.next;
---
+++ algorithmStudy/src/main/java/cn/edu/shu/leetcode21_40/solution_26/Solution.java
@@ -0,0 +1,48 @@
+package cn.edu.shu.leetcode21_40.solution_26;
+
+/**
+ * Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+ * <p>
+ * Do not allocate extra space for another array, you must do this in place with constant memory.
+ * <p>
+ * For example,
+ * Given input array nums = [1,1,2],
+ * <p>
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+ * It doesn't matter what you leave beyond the new length.
+ */
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 2) {
+            return nums.length;
+        }
+        int length = 1;
+        int last = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != last) {
+                length++;
+                last = nums[i];
+                nums[length - 1] = last;
+            }
+        }
+        return length;
+    }
+
+    public int removeDuplicates1(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int size = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[size]) {
+                nums[++size] = nums[i];
+            }
+        }
+        return size + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().removeDuplicates(new int[]{1, 1, 2}));
+    }
+}
